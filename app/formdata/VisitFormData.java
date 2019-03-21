0cd705182b565612f9b59b00d5cfa12da5e4b40d
Michael Shannon <msh152@uclive.ac.nz>
2019-03-21 15:40:07 +1300

    Merge remote-tracking branch 'origin' into story6task1294
    # Conflicts:
    #	app/views/main.scala.html
    #	public/stylesheets/main.css

--- app/formdata/VisitFormData.java
+++ app/formdata/VisitFormData.java
@@ -13,16 +13,22 @@ import java.util.List;
 @Constraints.Validate
 public class VisitFormData implements Constraints.Validatable<List<ValidationError>> {
 
+    public String destName;
+    public String arrival;
+    public String departure;
+    public String visitName;
+
+
     public VisitFormData(String destName,String arrival, String departure, String visitName) {
         this.destName = destName;
         this.arrival = arrival;
         this.departure = departure;
         this.visitName = visitName;
     }
-    public String destName;
-    public String arrival;
-    public String departure;
-    public String visitName;
+
+    public VisitFormData() {
+
+    }
 
     @Override
     public List<ValidationError> validate() {

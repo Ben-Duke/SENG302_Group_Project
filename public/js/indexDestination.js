648671bac79770fe1126b67de59315fe1efb11a5
Gavin Ong <gon12@uclive.ac.nz>
2019-04-11 16:35:37 +1200

    Created front end carousel for user photos. Each user photo has a button that allows them to link the photo to a destination which causes another modal to pop up.

--- public/js/indexDestination.js
+++ public/js/indexDestination.js
@@ -1,26 +1,29 @@
 //Function to search by name, type and country
 function searchDestination(){
     // Declare variables
-    var input, filter, table, tr, th, td, td2, i, txtValue, txtValue2, txtValue3;
-    input = document.getElementById("searchDestinations");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("privateDestinations");
-    tr = table.getElementsByTagName("tr");
-
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-        th = tr[i].getElementsByTagName("th")[0];
-        td = tr[i].getElementsByTagName("td")[0];
-        td2 = tr[i].getElementsByTagName("td")[1];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            txtValue2 = th.textContent || th.innerText;
-            txtValue3 = td2.textContent || td2.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1
-                || txtValue3.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
+    var input, elements, filter, tables, table, tr, th, td, td2, i, txtValue, txtValue2, txtValue3;
+    elements = document.getElementsByClassName("searchDestinations");
+    for(var a=0; a<elements.length; a++) {
+        input = elements[a];
+        filter = input.value.toUpperCase();
+        tables = document.getElementsByClassName("privateDestinations");
+        table = tables[a];
+        tr = table.getElementsByTagName("tr");
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) {
+            th = tr[i].getElementsByTagName("th")[0];
+            td = tr[i].getElementsByTagName("td")[0];
+            td2 = tr[i].getElementsByTagName("td")[1];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                txtValue2 = th.textContent || th.innerText;
+                txtValue3 = td2.textContent || td2.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1
+                    || txtValue3.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
             }
         }
     }
@@ -28,26 +31,30 @@ function searchDestination(){
 
 function searchPublicDestination(){
     // Declare variables
-    var input, filter, table, tr, th, td, td2, i, txtValue, txtValue2, txtValue3;
-    input = document.getElementById("searchPublicDestinations");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("publicDestinations");
-    tr = table.getElementsByTagName("tr");
+    var input, elements, filter, tables, table, tr, th, td, td2, i, txtValue, txtValue2, txtValue3;
+    elements = document.getElementsByClassName("searchPublicDestinations");
+    for(var a=0; a<elements.length; a++) {
+        input = elements[a];
+        filter = input.value.toUpperCase();
+        tables = document.getElementsByClassName("publicDestinations");
+        table = tables[a];
+        tr = table.getElementsByTagName("tr");
 
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-        th = tr[i].getElementsByTagName("th")[0];
-        td = tr[i].getElementsByTagName("td")[0];
-        td2 = tr[i].getElementsByTagName("td")[1];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            txtValue2 = th.textContent || th.innerText;
-            txtValue3 = td2.textContent || td2.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1
-                || txtValue3.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) {
+            th = tr[i].getElementsByTagName("th")[0];
+            td = tr[i].getElementsByTagName("td")[0];
+            td2 = tr[i].getElementsByTagName("td")[1];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                txtValue2 = th.textContent || th.innerText;
+                txtValue3 = td2.textContent || td2.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1
+                    || txtValue3.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
             }
         }
     }

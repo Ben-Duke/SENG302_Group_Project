75a8f1524a8de21bb4768f137e0dae679952a0e7
Logan Shaw <lsh73@uclive.ac.nz>
2019-04-21 12:04:51 +1200

    #story[243] Implemented functionality to show information on a Destination when clicking or hovering over a marker on the map. Got it working correctly with mouseover and mouseoff to briefly show the infoWindow, and for mouseclick to keep it shown even after mouseoff.

--- public/js/destination/destinationMap.js
+++ public/js/destination/destinationMap.js
@@ -2,7 +2,14 @@
  * ES6 source file for the google map on the Destination page.
  */
 
-window.globalMarkers = []; // global variable : a list of all marker objects on the map
+// global variable : a list of all markers info on the map
+// each element is in format :
+// {
+//     marker: marker,
+//     infoWindow: infoWindow,
+//     isClicked: false
+// }
+window.globalMarkers = [];
 
 /**
  * The callback function that is called after the google maps script src loads
@@ -20,10 +27,84 @@ function initMap() {
 }
 
 /**
- * Displays all visible destination markers to the google map. Add's the marker
- * objects to the window.globalMarkers global variable.
+ * Initiates all the event handlers for a google maps markers.
+ *
+ * @param markerIndex The index of the marker and window in window.globalMarkers
  */
-function initDestinationMarkers() {
+function initMarkerEventHandlers(markerIndex) {
+// event handler to open infoWindow on mouseout
+    window.globalMarkers[markerIndex].marker.addListener('mouseover', () => {
+        window.globalMarkers[markerIndex].infoWindow.open(window.globalMap,
+                                    window.globalMarkers[markerIndex].marker);
+    });
+
+    // event handler to close infoWindow on mouseout
+    window.globalMarkers[markerIndex].marker.addListener('mouseout', () => {
+
+        if (window.globalMarkers[markerIndex].isClicked) {
+            // user are clicked on current marker
+            // do nothing (dont close on mouseout)
+        } else {
+            // user hasn't explicitly clicked, so safe to close
+            window.globalMarkers[markerIndex].infoWindow.close(window.globalMap,
+                                    window.globalMarkers[markerIndex].marker);
+        }
+    });
+
+    // event handler to open infoWindow on click
+    window.globalMarkers[markerIndex].marker.addListener('click', () => {
+        window.globalMarkers[markerIndex].isClicked = true;
+        window.globalMarkers[markerIndex].infoWindow.open(window.globalMap,
+                                    window.globalMarkers[markerIndex].marker);
+    });
+}
+
+/**
+ * Initiates all the event handlers for a google maps infoWindows.
+ *
+ * @param markerIndex The index of the marker and window in window.globalMarkers
+ */
+function initInforWindowEventHandlers(markerIndex) {
+// event handler to handle infoWindow exit
+    window.globalMarkers[markerIndex].infoWindow.addListener('closeclick', () => {
+        window.globalMarkers[markerIndex].isClicked = false;
+        window.globalMarkers[markerIndex].infoWindow.close(window.globalMap,
+                                    window.globalMarkers[markerIndex].marker);
+    });
+}
+
+/**
+ * Gets the HTML for a Destinations infoWindow, for the google map.
+ *
+ * @param destination A JSON object of the Destination, from an AJAX query from
+ *  /users/destinations/getalljson endpoint.
+ *
+ * @returns {string} A String containing the HTML to display in the markers
+ *                   infoWindow.
+ */
+function getInfoWindowHTML(destination) {
+// create the destinations info window
+    const destinationName = destination.destName;
+    const destinationType = destination.destType;
+    const destinationCountry = destination.country;
+    const destinationDistrict = destination.district;
+
+    // uses a ES6 template string
+    const infoWindowHTML = `<h5>${destinationName}</h5>
+                            <div>Type: ${destinationType}</div>
+                            <div>Country: ${destinationCountry}</div>
+                            <div>District: ${destinationDistrict}</div>`;
+    return infoWindowHTML;
+}
+
+/**
+ * Gets the Icon (google maps api spec) for the Marker, depends on the Destintions
+ * privacy.
+ *
+ * @param isPublic A boolean, true if Destination is public, false otherwise
+ * @returns {icons.blueIcon|{url, scale}|icons.greenIcon} JSON of the icon
+ */
+function getMarkerIcon(isPublic) {
     const icons = {
         greenIcon: {
             url: 'https://maps.google.com/mapfiles/kml/paddle/grn-blank.png',
@@ -33,31 +114,54 @@ function initDestinationMarkers() {
             url: 'https://maps.google.com/mapfiles/kml/paddle/blu-blank.png',
             scale: 5
         }
-
     };
 
+    let selectedIcon;
+
+    if (isPublic) {
+        selectedIcon = icons.greenIcon;
+    } else {
+        selectedIcon = icons.blueIcon;
+    }
+
+    return selectedIcon;
+}
+
+/**
+ * Displays all visible destination markers to the google map. Add's the marker
+ * objects to the window.globalMarkers global variable.
+ */
+function initDestinationMarkers() {
     fetch('/users/destinations/getalljson', {
         method: 'GET'})
         .then(res => res.json())
-        .then(data => {
-            const destinations = data;
+        .then(destinations => {
+            let marker;
+            let infoWindow;
 
-            let customIcon;
-            for (destination of destinations) {
-                if (destination.isPublic) {
-                    customIcon = icons.greenIcon;
-                } else {
-                    customIcon = icons.blueIcon;
-                }
-
-                window.globalMarkers.push(new google.maps.Marker({
+            for (let index = 0; index < destinations.length; index++) {
+                marker = new google.maps.Marker({
                     position: {
-                        lat: destination.latitude,
-                        lng: destination.longitude
+                        lat: destinations[index].latitude,
+                        lng: destinations[index].longitude
                     },
                     map: window.globalMap,
-                    icon: customIcon
-                }));
+                    icon: getMarkerIcon(destinations[index].isPublic)
+                });
+
+                infoWindow = new google.maps.InfoWindow({
+                    content: getInfoWindowHTML(destinations[index])
+                });
+
+                //make the marker and infoWindow globals (persist in browser session)
+                window.globalMarkers.push({
+                    marker: marker,
+                    infoWindow: infoWindow,
+                    isClicked: false
+                });
+
+                initMarkerEventHandlers(index);
+                initInforWindowEventHandlers(index);
             }
         });
 }

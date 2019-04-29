07607d0feeb19e1f3dd14329c49c928fd651bd09
Logan Shaw <lsh73@uclive.ac.nz>
2019-04-29 12:21:39 +1200

    Added some quick jdoc to a few methods in ApplicationManager

--- app/controllers/ApplicationManager.java
+++ app/controllers/ApplicationManager.java
@@ -5,13 +5,22 @@ package controllers;
  */
 public class ApplicationManager {
 
-
     private static String userPhotoPath;
 
-    public static String getUserPhotoPath() {
+    /**
+     * Method to get the user photo path.
+     *
+     * @return A String representing the user photo URL
+     */
+    public static String getUserPhotoPath(){
         return userPhotoPath;
     }
 
+    /**
+     * Method to set the user photo path.
+     *
+     * @param userPhotoPath A String representing the user photo URL
+     */
     public static void setUserPhotoPath(String userPhotoPath) {
         ApplicationManager.userPhotoPath = userPhotoPath;
     }

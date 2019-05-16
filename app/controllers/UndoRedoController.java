851c9feadb49297be3a326590883d945e034851f
Noel Bisson <nbi21@uclive.ac.nz>
2019-05-16 19:36:14 +1200

    Add a custom unauthorized page
    #story[420]

--- app/controllers/UndoRedoController.java
+++ app/controllers/UndoRedoController.java
@@ -1,13 +1,21 @@
 package controllers;
 
+import models.User;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
+
+import views.html.responses.unauthorizedPage;
 
 /** Controller to handle undo/redo of actions */
 public class UndoRedoController extends Controller {
 
     public Result undo(Http.Request request) {
+        User user = User.getCurrentUser(request);
+        if (user == null) {
+            return unauthorized(unauthorizedPage.render());
+        }
+
         return ok();
     }
 

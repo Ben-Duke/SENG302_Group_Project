6f798e9ea81d7bb6fd472ebf9a1b8118348e9350
Luke Parkinson <lpa55@uclive.ac.nz>
2019-05-23 18:49:07 +1200

    Added documentation for deleting a treasure hunt #story[420]

--- app/models/commands/treasurehunts/DeleteTreasureHuntCommand.java
+++ app/models/commands/treasurehunts/DeleteTreasureHuntCommand.java
@@ -5,6 +5,7 @@ import accessors.UserAccessor;
 import models.TreasureHunt;
 import models.commands.UndoableCommand;
 
+/** Command to delete a treasure hunt */
 public class DeleteTreasureHuntCommand extends UndoableCommand {
     private TreasureHunt treasureHunt;
 
@@ -12,17 +13,26 @@ public class DeleteTreasureHuntCommand extends UndoableCommand {
         this.treasureHunt = treasureHunt;
     }
 
+    /**
+     * Executes the deletion of the treasure hunt
+     */
     @Override
     public void execute() {
         TreasureHuntAccessor.delete(treasureHunt);
     }
 
+    /**
+     * Undoes the deletion of the treasure hunt
+     */
     @Override
     public void undo() {
         treasureHunt = new TreasureHunt(treasureHunt);
         TreasureHuntAccessor.insert(treasureHunt);
     }
 
+    /**
+     * Redoes the deletion of the previously undone treasure hunt
+     */
     @Override
     public void redo() {
         execute();

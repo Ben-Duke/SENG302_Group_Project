cf564c7f6f064ae3d84e5a0e9884d3147dbfda1d
Jason Little <jpl62@uclive.ac.nz>
2019-05-24 13:41:57 +1200

    Setting up linking photo to destinaiton command #story[420]

---
+++ app/models/commands/Destinations/LinkPhotoDestinationCommand.java
@@ -0,0 +1,20 @@
+package models.commands.Destinations;
+
+import models.commands.UndoableCommand;
+
+public class LinkPhotoDestinationCommand extends UndoableCommand {
+
+
+
+    public void execute() {
+
+    }
+
+    public void undo() {
+
+    }
+
+    public void redo() {
+
+    }
+}

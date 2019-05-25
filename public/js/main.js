b14ce55b2362e570e56d8ca65a6418564e850482
Michael <msh152@uclive.ac.nz>
2019-05-25 16:43:28 +1200

    Added javadoc to main.js functions for undo/redo requests #story[420]

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,9 @@
 var user;
 
+/**
+ * Sets the global keyboard shortcuts for undo and redo
+ * Completes given undo or redo request
+ */
 $(document).keydown('undo_redo', function(e) {
     if ((e.ctrlKey && e.keyCode === 90) && e.shiftKey !== true) {
         undoRedoRequest("undo")
@@ -8,14 +12,26 @@ $(document).keydown('undo_redo', function(e) {
     };
 });
 
+/**
+ * Sets the onclick method for navigation bar undo button
+ * Completes given undo request
+ */
 $('#undoButton').click( function(e) {
     undoRedoRequest("undo")
 });
 
+/**
+ * Sets the onclick method for navigation bar redo button
+ * Completes given redo request
+ */
 $('#redoButton').click( function(e) {
     undoRedoRequest("redo")
 });
 
+/**
+ * Sends request to UndoRedoController which completes the given undo/redo request
+ * @param url Either Undo or Redo
+ */
 function undoRedoRequest(url){
     var token =  $('input[name="csrfToken"]').attr('value');
     $.ajaxSetup({
@@ -37,19 +53,10 @@ function undoRedoRequest(url){
 };
 
 
-function showMessage(message) {
-    var x = document.getElementById("snackbar");
-
-    // Add the "show" class to DIV
-    x.className = "show";
-    x.innerText = message;
-    console.log(x.innerText);
-    // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){
-        x.className = x.className.replace("show", "");
-        }, 3000);
-};
-
+/**
+ * Check if undo/redo request as been sent after location reload to show undo/redo message
+ * Sets session storage to false after
+ */
 $(function () {
         if ( sessionStorage.reloadAfterPageLoad !== false) {
             if (sessionStorage.reloadAfterPageLoad === "undo") {
@@ -61,4 +68,21 @@ $(function () {
         }
     }
 );
+
+/**
+ * Shows notification message for given undo/redo request
+ * @param message Either Undo or Redo
+ */
+function showMessage(message) {
+    var x = document.getElementById("snackbar");
+
+    // Add the "show" class to DIV
+    x.className = "show";
+    x.innerText = message;
+    console.log(x.innerText);
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){
+        x.className = x.className.replace("show", "");
+    }, 3000);
+};
 

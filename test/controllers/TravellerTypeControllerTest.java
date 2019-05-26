4d01f651fe84f70b543abe76fce863831c54b29f
Noel Bisson <nbi21@uclive.ac.nz>
2019-05-27 07:54:23 +1200

    Update 1 test class to use BaseTest - 1 test fails but this seems to always fail locally
    #story[420]

--- test/controllers/TravellerTypeControllerTest.java
+++ test/controllers/TravellerTypeControllerTest.java
@@ -3,19 +3,11 @@ package controllers;
 import models.Destination;
 import models.TravellerType;
 import models.User;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import play.db.Database;
-import play.db.Databases;
-import play.db.evolutions.Evolution;
-import play.db.evolutions.Evolutions;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.test.Helpers;
-import play.test.WithApplication;
-import utilities.TestDatabaseManager;
-
+import testhelpers.BaseTestWithApplicationAndDatabase;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,43 +16,7 @@ import static play.mvc.Http.Status.*;
 import static play.test.Helpers.GET;
 import static play.test.Helpers.route;
 
-public class TravellerTypeControllerTest extends WithApplication {
-
-    /**
-     * The fake database
-     */
-    Database database;
-
-    /**
-     * Sets up the fake database before each test
-     */
-    @Before
-    public void setupDatabase() {
-        database = Databases.inMemory();
-        Evolutions.applyEvolutions(database, Evolutions.forDefault(new Evolution(
-                1,
-                "create table test (id bigint not null, name varchar(255));",
-                "drop table test;"
-        )));
-
-
-        //Initialises a test user with name "testUser" and saves it to the database.
-        User user = new User("testUser");
-        user.save();
-        ApplicationManager.setIsTest(true);
-        TestDatabaseManager testDatabaseManager = new TestDatabaseManager();
-        testDatabaseManager.populateDatabase();
-    }
-
-    /**
-     * Clears the fake database after each test
-     */
-    @After
-    public void shutdownDatabase() {
-        Evolutions.cleanupEvolutions(database);
-        database.shutdown();
-    }
-
+public class TravellerTypeControllerTest extends BaseTestWithApplicationAndDatabase {
 
     /**
      * Unit test for rendering the traveller type page

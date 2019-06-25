37f12c55486b7295371b94d4a15a9a5150e0ea8a
Luke Parkinson <lpa55@uclive.ac.nz>
2019-06-25 16:50:41 +1200

    Duplicated the google places api to the create destinations page. This may change when the new redesign comes in #story[479]

--- public/js/destination/destinationMap.js
+++ public/js/destination/destinationMap.js
@@ -24,33 +24,11 @@ function initIndexDestinationMap() {
         zoom: 5
     });
 
-
-
-    var input = document.getElementById('placesAutocomplete');
-    var autocomplete = new google.maps.places.Autocomplete(input);
-
-    // Bind the map's bounds (viewport) property to the autocomplete object,
-    // so that the autocomplete requests use the current map bounds for the
-    // bounds option in the request.
-    autocomplete.bindTo('bounds', window.globalMap);
-
-    // Set the data fields to return when the user selects a place.
-    autocomplete.setFields(
-        ['address_components', 'geometry', 'icon', 'name']);
-
-    autocomplete.addListener('place_changed', function() {
-        var place = autocomplete.getPlace();
-
-        console.log(place);
-
-    });
-
+    initPlacesAutocomplete();
 
     initDestinationMarkers();
     initMapLegend();
 }
-
-
 
 
 /**
@@ -116,8 +94,34 @@ function initCreateDestinationMap() {
         document.getElementById("longitude").value = event.latLng.lng();
     });
 
+    initPlacesAutocomplete();
+
     initDestinationMarkers();
     initMapLegend();
+}
+
+/**
+ * Initialises the google places api auto-complete box
+ */
+function initPlacesAutocomplete() {
+    const input = document.getElementById('placesAutocomplete');
+    const autocomplete = new google.maps.places.Autocomplete(input);
+
+    // Bind the map's bounds (viewport) property to the autocomplete object,
+    // so that the autocomplete requests use the current map bounds for the
+    // bounds option in the request.
+    autocomplete.bindTo('bounds', window.globalMap);
+
+    // Set the data fields to return when the user selects a place.
+    autocomplete.setFields(
+        ['address_components', 'geometry', 'icon', 'name']);
+
+    autocomplete.addListener('place_changed', function() {
+        const place = autocomplete.getPlace();
+
+        console.log(place);
+
+    });
 }
 
 /**

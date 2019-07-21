9eba1b74d854e07c280a13602b617a8947d28ec2
Priyesh <psh76@uclive.ac.nz>
2019-07-22 02:40:15 +1200

    Partly implemented lightbox with 4 dummy images. Next step is to make it adaptable to view several number of images from the albums. #implement #story[423]

--- public/js/album/albumView.js
+++ public/js/album/albumView.js
@@ -19,7 +19,11 @@ function getAlbum(userId, albumId, isOwner){
             success: function(albumData){
                 for (i=0; i<albumData.length; i++) {
                     var img1 = document.createElement("img");
+                    img1.setAttribute('id', i+1);
                     img1.src = path + albumData[i];
+                    img1.class = "hover-shadow";
+                    img1.addEventListener('click', openModal);
+                    img1.addEventListener('click', currentSlide(i+1));
                     if (i%4==0) {
                         document.getElementById('col1').appendChild(img1);
                     } else if (i%4==1){
@@ -35,3 +39,49 @@ function getAlbum(userId, albumId, isOwner){
     console.log("done");
 }
 
+// MDB Lightbox Init
+$(function () {
+$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
+
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}

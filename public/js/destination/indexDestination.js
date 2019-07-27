f73d0c6cd15880babccc944e3248c8495f22dde9
Michael <msh152@uclive.ac.nz>
2019-07-27 12:24:25 +1200

    Front end view for tags when viewing a single destination #story[485]

--- public/js/destination/indexDestination.js
+++ public/js/destination/indexDestination.js
@@ -236,6 +236,45 @@ function populateViewDestinationModal()
                     $('#travellerTypes').html(outerDivNode);
                 }
             });
+            let url = '/destinations/' + getIdFromRow + '/tags';
+            $.ajax({
+                data : '',
+                contentType : 'application/json',
+                type: 'GET',
+                url: url,
+                success: function(data, textStatus, xhr){
+                    if(xhr.status == 200) {
+                        for(let i = 0; i < data.length; i++){
+                            // $('#travellerTypes').append("<p>Traveller type(s):</p>");
+                            var outerDivNode = document.createElement("div");
+                            // outerDivNode.classList.
+                            var parNode = document.createElement("p");
+                            var parTextNode = document.createTextNode("Tags(s)");
+                            parNode.appendChild(parTextNode);
+                            outerDivNode.appendChild(parNode);
+                            var ulNode = document.createElement("ul");
+                            ulNode.classList.add("list-group");
+                            outerDivNode.appendChild(ulNode);
+
+                            $.each(data, function(index, element){
+                                var liNode = document.createElement("li");
+                                liNode.classList.add("list-group-item");
+                                var liTextNode = document.createTextNode(element["name"]);
+                                liNode.appendChild(liTextNode);
+                                ulNode.appendChild(liNode);
+                            });
+                            // outerDivNode.classList.add("col-md-offset-1");
+                            // outerDivNode.classList.add("col-md-4");
+                            $('#trip-tags-div').html(outerDivNode);
+                        }
+
+
+                    }
+                    else{
+                        console.log("Got not successful")
+                    }
+                    },
+                });
             $.ajax({
                 type: 'GET',
                 url: '/users/destinations/photos/' + getIdFromRow,
@@ -472,6 +511,9 @@ $('#removePhotoButton').click(function(e){
         }
     });
 });
+
+
+
 
 /**
  * Called when the user clicks the create destination button.

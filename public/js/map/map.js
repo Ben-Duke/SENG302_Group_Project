4d7e87048885bf02e7bb51072ef6e3aefb30bcee
jco165 <jco165@uclive.ac.nz>
2019-08-06 21:42:57 +1200

    Removed overlapping js issues with map home and destination map to allow visits to be added to trips and new trips to be created according to what is selected. Still need dynamic UI updates when the button is pressed. #story[516]

--- public/js/map/map.js
+++ public/js/map/map.js
@@ -1,4 +1,5 @@
 var visitArray = [];
+window.globalMarkers = [];
 const updateVisitDateUrl = "/user/trips/visit/dates/";
 const colors = ['6b5b95', 'feb236', 'd64161', 'ff7b25',
     '6b5b95', '86af49', '3e4444', 'eca1a6', 'ffef96', 'bc5a45', 'c1946a'];
@@ -16,7 +17,7 @@ let isNewTrip = false;
  * @returns {string} A String containing the HTML to display in the markers
  *                   infoWindow.
  */
-function getInfoWindowHTML(destination) {
+function getMapInfoWindowHTML(destination) {
 // create the destinations info window
     const destinationName = destination.destName;
     const destinationType = destination.destType;
@@ -33,8 +34,7 @@ function getInfoWindowHTML(destination) {
                       <div>${destinationType}</div>
                       <div>District: ${destinationDistrict}</div>
                       <div>${destinationCountry}</div>
-                      <div><button id="addToTripButton" onclick="addSelectedToVisitToTrip(${destination.destId})">Add to trip</button></div>
-                      <script src="indexDestination.js"></script>`;
+                      <div><button id="addToTripButton" onclick="addSelectedToVisitToTrip(${destination.destId})">Add to trip</button></div>`;
 
     return infoWindowHTML;
 }
@@ -129,7 +129,7 @@ function addSelectedToVisitToTrip(destId){
                 //groupDiv.appendChild(tripCheckBox);
                 //groupDiv.appendChild(mapLabel);
                 addTripRoutes(data.tripId);
-                displayTrip(data.tripId, data.latitude, data.longitude);
+                //displayTrip(data.tripId, data.latitude, data.longitude);
 
 
             },
@@ -329,6 +329,7 @@ function displayTrip(tripId, startLat, startLng) {
         isNewTrip = false;
         currentlyDisplayedTripId = undefined;
     }
+    console.log(tripId);
     let checkBox = document.getElementById("Toggle" + tripId);
     if (checkBox.checked === true) {
         if (currentlyDisplayedTripId !== undefined) {
@@ -697,5 +698,131 @@ function initPlacesAutocompleteSearch() {
         });
 
 
+    });
+}
+
+/**
+ * Displays all visible destination markers to the google map. Add's the marker
+ * objects to the window.globalMarkers global variable.
+ */
+function initDestinationMarkers() {
+    fetch('/users/destinations/getalljson', {
+        method: 'GET'})
+        .then(res => res.json())
+        .then(destinations => {
+            let marker;
+            let infoWindow;
+            // console.log(destinations);
+            for (let index = 0; index < destinations.length; index++) {
+                marker = new google.maps.Marker({
+                    position: {
+                        lat: destinations[index].latitude,
+                        lng: destinations[index].longitude
+                    },
+                    map: window.globalMap,
+                    icon: getMarkerIcon(destinations[index].isPublic)
+                });
+
+                infoWindow = new google.maps.InfoWindow({
+                    content: getMapInfoWindowHTML(destinations[index])
+                });
+
+                //make the marker and infoWindow globals (persist in browser session)
+                window.globalMarkers.push({
+                    marker: marker,
+                    infoWindow: infoWindow,
+                    isClicked: false
+                });
+
+                initMarkerEventHandlers(index);
+                initInforWindowEventHandlers(index);
+            }
+        });
+}
+
+/**
+ * Gets the Icon (google maps api spec) for the Marker, depends on the Destintions
+ * privacy.
+ *
+ * @param isPublic A boolean, true if Destination is public, false otherwise
+ * @returns {icons.blueIcon|{url, scale}|icons.greenIcon} JSON of the icon
+ */
+function getMarkerIcon(isPublic) {
+    const icons = getAllMarkerIcons();
+
+    let selectedIcon;
+
+    if (isPublic) {
+        selectedIcon = icons.greenIcon;
+    } else {
+        selectedIcon = icons.blueIcon;
+    }
+
+    return selectedIcon;
+}
+
+/**
+ * Gets a JSON of all marker icons.
+ *
+ * @returns {{greenIcon: {url: string, name: string}, blueIcon: {url: string, name: string}}}
+ */
+function getAllMarkerIcons() {
+    const icons = {
+        greenIcon: {
+            url: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
+            name: 'Public Destination'
+        },
+        blueIcon: {
+            url: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+            name: 'Private Destination'
+        }
+    };
+    return icons;
+}
+
+/**
+ * Initiates all the event handlers for a google maps markers.
+ *
+ * @param markerIndex The index of the marker and window in window.globalMarkers
+ */
+function initMarkerEventHandlers(markerIndex) {
+// event handler to open infoWindow on mouseout
+    window.globalMarkers[markerIndex].marker.addListener('mouseover', () => {
+        window.globalMarkers[markerIndex].infoWindow.open(window.globalMap,
+                                    window.globalMarkers[markerIndex].marker);
+    });
+
+    // event handler to close infoWindow on mouseout
+    window.globalMarkers[markerIndex].marker.addListener('mouseout', () => {
+
+        if (window.globalMarkers[markerIndex].isClicked) {
+            // user are clicked on current marker
+            // do nothing (dont close on mouseout)
+        } else {
+            // user hasn't explicitly clicked, so safe to close
+            window.globalMarkers[markerIndex].infoWindow.close(window.globalMap,
+                                    window.globalMarkers[markerIndex].marker);
+        }
+    });
+
+    // event handler to open infoWindow on click
+    window.globalMarkers[markerIndex].marker.addListener('click', () => {
+        window.globalMarkers[markerIndex].isClicked = true;
+        window.globalMarkers[markerIndex].infoWindow.open(window.globalMap,
+                                    window.globalMarkers[markerIndex].marker);
+    });
+}
+
+/**
+ * Initiates all the event handlers for a google maps infoWindows.
+ *
+ * @param markerIndex The index of the marker and window in window.globalMarkers
+ */
+function initInforWindowEventHandlers(markerIndex) {
+// event handler to handle infoWindow exit
+    window.globalMarkers[markerIndex].infoWindow.addListener('closeclick', () => {
+        window.globalMarkers[markerIndex].isClicked = false;
+        window.globalMarkers[markerIndex].infoWindow.close(window.globalMap,
+                                    window.globalMarkers[markerIndex].marker);
     });
 }

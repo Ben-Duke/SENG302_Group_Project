d3999e00ea56554cfc265bec52964b06a4cc25ae
Gavin Ong <gon12@uclive.ac.nz>
2019-08-15 18:36:23 +1200

    Fixed imports  #pair[gon12,lpa55] #story[518]

--- app/accessors/TagAccessor.java
+++ app/accessors/TagAccessor.java
@@ -1,12 +1,8 @@
 package accessors;
 
 import models.*;
-
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 
 public class TagAccessor {

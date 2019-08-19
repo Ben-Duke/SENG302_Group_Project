d41988efd87492558f6aadc4cf6c55bbb697a777
Priyesh <psh76@uclive.ac.nz>
2019-08-20 02:55:06 +1200

    Implemented complete pagination. Faced several issues assigning the onclick method to the button as it was executing it infinitely even without clicking it. Still need to make it a bit better. #implement #story[547]

--- public/js/events/indexEvents.js
+++ public/js/events/indexEvents.js
@@ -1,6 +1,7 @@
 getEventsData(-43.53, 172.620278, 'Christchurch', 1);
 
 function getEventsData(latitude, longitude, place, pageNum) {
+
     const offset = (pageNum - 1) * 20;
     $.ajax({
         type: 'GET',
@@ -9,7 +10,12 @@ function getEventsData(latitude, longitude, place, pageNum) {
         success: (eventData) => {
             const count = eventData["@attributes"].count;
             const events = eventData.events;
-            console.log(events);
+            if (pageNum > 1) {
+                let eventsPage = document.getElementById("events-results");
+                while (eventsPage.childNodes.length > 0) {
+                    eventsPage.removeChild(eventsPage.childNodes[0]);
+                }
+            }
             displayEvents(events);
             addPagination(count);
         }
@@ -24,13 +30,11 @@ function displayEvents(events) {
         mediaLeft.classList.add("media-left");
         const eventImageLink = document.createElement("a");
         eventImageLink.setAttribute("target", "_blank");
-        eventImageLink.setAttribute("href", events[i].images.images[0].transforms.transforms[3].url);
+        const lastImage = events[i].images.images[0].transforms["@attributes"].count - 1;
+        eventImageLink.setAttribute("href", events[i].images.images[0].transforms.transforms[lastImage].url);
         const eventImage = document.createElement("img");
         eventImage.classList.add("img-thumbnail");
-        console.log(events[i]);
-        eventImage.setAttribute("src", events[i].images.images[0].transforms.transforms[3].url);
-
-
+        eventImage.setAttribute("src", events[i].images.images[0].transforms.transforms[lastImage].url);
         const mediaBody = document.createElement("div");
         mediaBody.classList.add("media-body")
         const eventLink = document.createElement("a");
@@ -59,14 +63,18 @@ function displayEvents(events) {
         mediaRow.appendChild(mediaBody);
         mediaRow.appendChild(document.createElement("hr"));
         document.getElementById("events-results").appendChild(mediaRow);
-
     }
 }
 
 function addPagination(count) {
+    latitudes = -43.53;
+    longitudes = 172.620278;
+    places = 'Christchurch';
     for (let i=0; i < count; i+=20) {
         const pageButton = document.createElement("Button");
-        pageButton.innerText = ((i/20)+1);
+        const pageNum = (i/20)+1;
+        pageButton.innerText = (i/20)+1;
+        pageButton.setAttribute("onClick", `getEventsData(-43.53, 172.620278, 'Christchurch', ${pageNum})`);
         document.getElementById("events-results").appendChild(pageButton);
     }
 }

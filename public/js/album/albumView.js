fcffe5fef15beae527dfa340af913857d7526b9b
Luke Parkinson <lpa55@uclive.ac.nz>
2019-08-20 11:25:42 +1200

    Readded edit dropdown for photos. Deletion is currently not working #story[518]

--- public/js/album/albumView.js
+++ public/js/album/albumView.js
@@ -1036,6 +1036,25 @@ function deleteAndUnlinkPhoto() {
     });
 }
 
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function openDropdown() {
+    document.getElementById("optionsDropdown").classList.toggle("show");
+}
+
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var openDropdown = document.getElementsByClassName("optionsDropdown");
+        if (openDropdown.classList !== undefined) {
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+
+};
 
 $('#photo-upload').click(function (eve){
     let searchBar = document.getElementById("album-search-photo");

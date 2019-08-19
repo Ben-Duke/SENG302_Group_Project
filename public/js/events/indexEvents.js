29d2dace09120ccafaefa47f2fc1363cf6a7d7b0
Priyesh <psh76@uclive.ac.nz>
2019-08-19 21:26:44 +1200

    Implemented javascript method to populate events data on the events page. #implement #story[547]

--- public/js/events/indexEvents.js
+++ public/js/events/indexEvents.js
@@ -9,9 +9,50 @@ function getEventsData(latitude, longitude, place, pageNum) {
         success: (eventData) => {
             const count = eventData["@attributes"].count;
             const events = eventData.events;
+            console.log(events);
+            displayEvents(events);
             addPagination(count);
         }
     });
+}
+
+function displayEvents(events) {
+    for (let i=0; i < events.length; i++) {
+        const mediaRow = document.createElement("div");
+        mediaRow.classList.add("row", "media")
+        const mediaLeft = document.createElement("div");
+        mediaLeft.classList.add("media-left");
+        const eventImageLink = document.createElement("a");
+        eventImageLink.setAttribute("href", "#");
+        const eventImage = document.createElement("img-thumbnail");
+        eventImage.setAttribute("src", "/assets/images/destinationPlaceHolder.png");
+
+
+        const mediaBody = document.createElement("div");
+        mediaBody.classList.add("media-body")
+        const eventName = document.createElement("h4");
+        eventName.classList.add("media-heading");
+        eventName.innerText = events[i]["name"];
+        const eventDateTime = document.createElement("p");
+        eventDateTime.innerText = events[i].datetime_start + "-" + events[i].datetime_end;
+        const eventAddress = document.createElement("p");
+        eventAddress.innerText = events[i].address;
+        const eventDescription = document.createElement("p");
+        eventDescription.innerText = events[i].description;
+
+        mediaBody.appendChild(eventName);
+        mediaBody.appendChild(eventDateTime);
+        mediaBody.appendChild(eventAddress);
+        mediaBody.appendChild(eventDescription);
+
+        eventImageLink.appendChild(eventImage);
+        mediaLeft.appendChild(eventImageLink);
+        mediaRow.appendChild(mediaLeft);
+        mediaRow.appendChild(mediaBody);
+
+        document.getElementById("events-results").appendChild(mediaRow);
+
+    }
 }
 
 function addPagination(count) {
@@ -21,3 +62,4 @@ function addPagination(count) {
         document.getElementById("events-results").appendChild(pageButton);
     }
 }
+

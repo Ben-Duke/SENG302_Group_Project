7ca43e50f3ceb112420475714bc195950407c670
Noel Bisson <nbi21@uclive.ac.nz>
2019-08-21 16:41:26 +1200

    Make sql script be read in by a file reader
    #story[546]

---
+++ app/utilities/SqlFileReader.java
@@ -0,0 +1,35 @@
+package utilities;
+
+import org.slf4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class SqlFileReader {
+    private String filepath;
+    private final Logger logger = UtilityFunctions.getLogger();
+
+    public SqlFileReader(String filename) {
+        filepath = Paths.get(".").toAbsolutePath().normalize().toString() + '/' + filename;
+    }
+
+    public String readFile() {
+        String output = "";
+        try (FileReader fileReader = new FileReader(this.filepath);) {
+            BufferedReader bufferedReader = new BufferedReader(fileReader, 8192);
+
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                output += line.trim() + '\n';
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            logger.error(String.format("Failed to read file %s", e.toString()));
+        }
+
+        logger.debug(output);
+        return output;
+    }
+}

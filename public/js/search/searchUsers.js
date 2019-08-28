16dc5769be239f974d43c79f3e6e1a0133e9ec52
Luke Parkinson <lpa55@uclive.ac.nz>
2019-08-28 16:07:28 +1200

    Created searchUsers.js

---
+++ public/js/search/searchUsers.js
@@ -0,0 +1,71 @@
+//n = 0, sorting by email
+//n = 2, sorting by gender
+//n = 3, sorting by the travelers first nationality
+//n = 4, sorting by date of birth
+//n = 5, sorting by the travelers first traveler type
+function sortTable(n) {
+    let table, rows, switching, i, x, y, shouldSwitch, dir, switchCount = 0;
+    table = document.getElementById("searchResultsTable");
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /* Loop through all table rows (except the
+        first, which contains table headers): */
+        for (i = 1; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            /* Get the two elements you want to compare,
+            one from current row and one from the next: */
+            if (n === 2 || n === 4) {
+                /*This is for the nationality and traveler type columns, the first traveler type
+                or nationality is taken */
+                x = rows[i].getElementsByTagName("TD")[n].getElementsByTagName("P")[0];
+                y = rows[i + 1].getElementsByTagName("TD")[n].getElementsByTagName("P")[0];
+            } else {
+                x = rows[i].getElementsByTagName("TD")[n];
+                y = rows[i + 1].getElementsByTagName("TD")[n];
+            }
+
+            // console.log(dir);
+            // console.log(x.innerHTML.toLowerCase() +" "+ y.innerHTML.toLowerCase());
+            // console.log(x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase());
+
+            /* Check if the two rows should switch place,
+            based on the direction, asc or desc: */
+            if (dir === "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir === "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            // Each time a switch is done, increase this count by 1:
+            switchCount ++;
+        } else {
+            /* If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again. */
+            if (switchCount === 0 && dir === "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+}

9a8a6b03c03207f32afe8c71626c6bbc2445dbb7
Noel Bisson <nbi21@uclive.ac.nz>
2019-09-09 15:51:58 +1200

    Fix 500 error, make 3.sql smaller to see if it runs faster that way.
    #pair[nbi21, bad38]
    #story[617]

--- Extras/generate 10k users.py
+++ Extras/generate 10k users.py
@@ -11,4 +11,3 @@ with open('users_from_api.json') as my_file:
 print("INSERT INTO 'user' ('userid', 'email', 'password_hash', 'date_of_birth', 'gender','f_name', 'l_name', 'undo_redo_error', 'is_admin', 'creation_date') VALUES ")
 for user in users:
         print(f"({1}, '{ user['email']}', '', {user['dob']}, '{user['gender'] }', '{ user['first_name']}', '{ user['last_name']}', 0, 0, 2019-07-26 03:59:17')")
-

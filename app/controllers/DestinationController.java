b81ecb94b1ef82e9c2e9a04521290da0e160e809
Logan Shaw <lsh73@uclive.ac.nz>
2019-09-11 11:52:31 +1200

    #story[617] #pair[lsh73,bad38] We created a new route to get a paginated jsonArray of public destinations, with the complementary controller method to handle it and an accessor method to get the data from the backend. We learned about offsetting and limiting sql query results.

--- app/controllers/DestinationController.java
+++ app/controllers/DestinationController.java
@@ -7,9 +7,14 @@ import accessors.UserPhotoAccessor;
 import accessors.UserPhotoAccessor;
 import accessors.TreasureHuntAccessor;
 import accessors.UserPhotoAccessor;
+import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import factories.DestinationFactory;
 import factories.TravellerTypeFactory;
 import factories.UserFactory;
@@ -37,6 +42,7 @@ import views.html.users.destination.*;
 import javax.inject.Inject;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -1023,6 +1029,36 @@ public class DestinationController extends Controller {
         } else {
             return redirect(routes.UserController.userindex());
         }
+    }
+
+    /**
+     * Gets a paginated jsonArray of public destinations based on an offset and quantity
+     * @param request the HTTP request
+     * @param offset an integer representing the number of destinations to skip before sending
+     * @param quantity an integer representing the maximum length of the jsonArray
+     * @return a Result object containing the destinations JSON in it's body
+     */
+    public Result getPaginatedPublicDestinations(Http.Request request, int offset, int quantity) {
+        int MAX_QUANTITY = 1000;
+
+        User user = User.getCurrentUser(request);
+        if (user == null) {
+            return redirect(routes.UserController.userindex());
+        }
+
+        if (MAX_QUANTITY < quantity) {
+            String errorStr = "query parameter 'quantity' exceeded maximum " +
+                    "allowed int: " + MAX_QUANTITY;
+
+            ObjectNode jsonError = (new ObjectMapper()).createObjectNode();
+            jsonError.put("error", errorStr);
+            jsonError.put("quantityLimit", MAX_QUANTITY);
+            return badRequest(Json.toJson(jsonError));
+        }
+
+        List<Destination> destinations = DestinationAccessor
+                .getPaginatedPublicDestinations(offset, quantity);
+        return ok(Json.toJson(destinations));
     }
 
     /**

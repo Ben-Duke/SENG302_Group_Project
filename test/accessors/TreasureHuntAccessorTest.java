a69cf1fc3361d4f02c9a66dcc89f2ef298cbbcce
Logan Shaw <lsh73@uclive.ac.nz>
2019-09-19 10:30:25 +1200

    #story[622] Added unit tests for getCountOpenTreasureHunts() method of TreasureHuntAccessor.

--- test/accessors/TreasureHuntAccessorTest.java
+++ test/accessors/TreasureHuntAccessorTest.java
@@ -93,11 +93,6 @@ public class TreasureHuntAccessorTest extends BaseTestWithApplicationAndDatabase
     }
 
     @Test
-    public void getCountOpenTreasureHunts() {
-        fail();
-    }
-
-    @Test
     /**
      * Checks getCountUsersownTreasureHunts() returns 0 when there are no treasure hunts
      * in the application.
@@ -171,5 +166,165 @@ public class TreasureHuntAccessorTest extends BaseTestWithApplicationAndDatabase
         TreasureHuntAccessor.insert(ownTHunt);
 
         assertEquals(1, TreasureHuntAccessor.getCountUsersownTreasureHunts(user));
+    }
+
+    @Test
+    /**
+     * Checks getCountOpenTreasureHunts returns zero when there are no treasure
+     * hunts in the  application.
+     */
+    public void getCountOpenTreasureHunts_zeroHunts_checkReturnZero() {
+        TestDatabaseManager dbManager = new TestDatabaseManager();
+        dbManager.clearAllData();
+
+        assertEquals(0, TreasureHuntAccessor.getCountOpenTreasureHunts());
+    }
+
+    @Test
+    /**
+     * Checks getCountOpenTreasureHunts returns zero when the only treasure hunt
+     * closed before the present day.
+     */
+    public void getCountOpenTreasureHunts_onlyWellBeforeTodayHunts_checkReturnZero() throws EbeanDateParseException {
+        TestDatabaseManager dbManager = new TestDatabaseManager();
+        dbManager.clearAllData();
+
+        User user = new User("test@test.com", "sasdsad");
+        UserAccessor.insert(user);
+
+        Destination destination = new Destination("test",
+                "test", "test", "New Zealand",
+                32.2, 22.1, user);
+        DestinationAccessor.insert(destination);
+
+        TreasureHunt tHunt = new TreasureHunt("test", "test",
+                destination, "2019-01-01", "2019-02-02", user);
+        TreasureHuntAccessor.insert(tHunt);
+
+        assertEquals(0, TreasureHuntAccessor.getCountOpenTreasureHunts());
+    }
+
+    @Test
+    /**
+     * Checks getCountOpenTreasureHunts returns zero when the only hunt starts
+     * and ends in the future.
+     */
+    public void getCountOpenTreasureHunts_onlyWellAfterTodayHunts_checkReturnZero() throws EbeanDateParseException {
+        TestDatabaseManager dbManager = new TestDatabaseManager();
+        dbManager.clearAllData();
+
+        User user = new User("test@test.com", "sasdsad");
+        UserAccessor.insert(user);
+
+        Destination destination = new Destination("test",
+                "test", "test", "New Zealand",
+                32.2, 22.1, user);
+        DestinationAccessor.insert(destination);
+
+        TreasureHunt tHunt = new TreasureHunt("test", "test",
+                destination, LocalDate.now().plusYears(1),
+                LocalDate.now().plusYears(2), user);
+        TreasureHuntAccessor.insert(tHunt);
+
+        assertEquals(0, TreasureHuntAccessor.getCountOpenTreasureHunts());
+    }
+
+    @Test
+    /**
+     * Checks getCountOpenTreasureHunts returns zero when the only treasure hunt
+     * closed on the present day.
+     */
+    public void getCountOpenTreasureHunts_huntClosedToday_checkReturnZero() throws EbeanDateParseException {
+        TestDatabaseManager dbManager = new TestDatabaseManager();
+        dbManager.clearAllData();
+
+        User user = new User("test@test.com", "sasdsad");
+        UserAccessor.insert(user);
+
+        Destination destination = new Destination("test",
+                "test", "test", "New Zealand",
+                32.2, 22.1, user);
+        DestinationAccessor.insert(destination);
+
+        TreasureHunt tHunt = new TreasureHunt("test", "test",
+                destination, LocalDate.now().minusDays(5), LocalDate.now(), user);
+        TreasureHuntAccessor.insert(tHunt);
+
+        assertEquals(0, TreasureHuntAccessor.getCountOpenTreasureHunts());
+    }
+
+    @Test
+    /**
+     * Checks getCountOpenTreasureHunts returns zero when the only treasure hunt
+     * starts after the present day.
+     */
+    public void getCountOpenTreasureHunts_huntstartsAfterToday_checkReturnZero() throws EbeanDateParseException {
+        TestDatabaseManager dbManager = new TestDatabaseManager();
+        dbManager.clearAllData();
+
+        User user = new User("test@test.com", "sasdsad");
+        UserAccessor.insert(user);
+
+        Destination destination = new Destination("test",
+                "test", "test", "New Zealand",
+                32.2, 22.1, user);
+        DestinationAccessor.insert(destination);
+
+        TreasureHunt tHunt = new TreasureHunt("test", "test",
+                destination, LocalDate.now().plusDays(5),
+                LocalDate.now().plusDays(10), user);
+        TreasureHuntAccessor.insert(tHunt);
+
+        assertEquals(0, TreasureHuntAccessor.getCountOpenTreasureHunts());
+    }
+
+    @Test
+    /**
+     * Checks getCountOpenTreasureHunts returns one when the only treasure hunt
+     * starts on the present day.
+     */
+    public void getCountOpenTreasureHunts_huntStartsToday_checkReturnOne() throws EbeanDateParseException {
+        TestDatabaseManager dbManager = new TestDatabaseManager();
+        dbManager.clearAllData();
+
+        User user = new User("test@test.com", "sasdsad");
+        UserAccessor.insert(user);
+
+        Destination destination = new Destination("test",
+                "test", "test", "New Zealand",
+                32.2, 22.1, user);
+        DestinationAccessor.insert(destination);
+
+        TreasureHunt tHunt = new TreasureHunt("test", "test",
+                destination, LocalDate.now(),
+                LocalDate.now().plusDays(10), user);
+        TreasureHuntAccessor.insert(tHunt);
+
+        assertEquals(1, TreasureHuntAccessor.getCountOpenTreasureHunts());
+    }
+
+    @Test
+    /**
+     * Checks getCountOpenTreasureHunts returns one when the only treasure hunt
+     * starts on the present day.
+     */
+    public void getCountOpenTreasureHunts_huntStartsBeforeTodayFinishesAfterToday_checkReturnOne() throws EbeanDateParseException {
+        TestDatabaseManager dbManager = new TestDatabaseManager();
+        dbManager.clearAllData();
+
+        User user = new User("test@test.com", "sasdsad");
+        UserAccessor.insert(user);
+
+        Destination destination = new Destination("test",
+                "test", "test", "New Zealand",
+                32.2, 22.1, user);
+        DestinationAccessor.insert(destination);
+
+        TreasureHunt tHunt = new TreasureHunt("test", "test",
+                destination, LocalDate.now().minusDays(2),
+                LocalDate.now().plusDays(10), user);
+        TreasureHuntAccessor.insert(tHunt);
+
+        assertEquals(1, TreasureHuntAccessor.getCountOpenTreasureHunts());
     }
 }

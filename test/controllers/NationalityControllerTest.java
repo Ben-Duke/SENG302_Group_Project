114bccef0da7c4f608638d5911695a28277c6a8b
Gavin Ong <gon12@uclive.ac.nz>
2019-09-20 16:05:14 +1200

    Added unit tests for new backend methods #story[617]

--- test/controllers/NationalityControllerTest.java
+++ test/controllers/NationalityControllerTest.java
@@ -1,13 +1,27 @@
 package controllers;
 
+import accessors.NationalityAccessor;
 import org.junit.Test;
+import play.libs.Json;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.test.Helpers;
 import testhelpers.BaseTestWithApplicationAndDatabase;
 
 import static org.junit.Assert.*;
+import static play.test.Helpers.GET;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.route;
 
 public class NationalityControllerTest extends BaseTestWithApplicationAndDatabase {
 
     @Test
     public void getAllNationalities() {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method(GET)
+                .uri("/nationalities").session("connected", "4");
+        Result result = route(app, request);
+        assertEquals(NationalityAccessor.getAll().size(),
+                Json.parse(contentAsString(result)).size());
     }
 }
